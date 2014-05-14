cd2587bb75f336933ea679199c382cb7de7dc915
Antonin Delpeuch <antonin@delpeuch.eu>
2014-05-14 14:41:56 +0100

    Added bounding boxes for selection

--- DrawGraph.hs
+++ DrawGraph.hs
@@ -1,12 +1,31 @@
 module DrawGraph where
 
-import Data.List
+import Data.List as List
+import Data.Set as Set
+
 import Graphics.Rendering.Cairo
 import qualified Data.Map.Strict as Map
 import OpenGraph as OG
 import Control.Concurrent.MVar
+import Graphics.UI.Gtk.Gdk.EventM
+import Graphics.UI.Gtk.Abstract.Widget
 
 type GraphPresentation = Map.Map OId (Double,Double)
+
+data DrawingState = DSelect | DMoving OId | DNode | DEdge | DDrawing OPath
+     deriving (Eq)
+
+data BoundingBox = BBox Double Double Double Double
+     deriving (Eq,Ord)
+
+data ElemSelection = SelectNode OId | SelectEdge OPath | NoSelection
+
+data GrapState = GraphState {
+       totalGraph :: OGraph,
+       presentation :: GraphPresentation,
+       selection :: ElemSelection,
+       nodeBB :: (Set.Set (OId,BoundingBox)),
+       gateBB :: (Set.Set (OPath,BoundingBox)) }
 
 -- Drawing constants
 nodeSemiHeight = 20
@@ -23,7 +42,7 @@ bottomOuterGates = 400
 seqInt 0 accu = accu
 seqInt n accu = seqInt (n-1) (n:accu)
 
-doList f = foldl (\ accu elem -> accu >> (f elem)) (return ())
+doList f = List.foldl (\ accu elem -> accu >> (f elem)) (return ())
 
 -- drawGate nInputs nOutputs :: Render ()
 drawNode posX posY 0 0 = do
@@ -31,7 +50,7 @@ drawNode posX posY 0 0 = do
     setLineWidth 1
     
     moveTo posX posY
-    relMoveTo 0 nodeSemiHeight
+    relMoveTo 0 (-nodeSemiHeight)
     relLineTo nodeSemiHeight nodeSemiHeight
     relLineTo (-nodeSemiHeight) nodeSemiHeight
     relLineTo (-nodeSemiHeight) (-nodeSemiHeight)
@@ -44,7 +63,7 @@ drawNode posX posY 0 0 = do
 drawNode posX posY nInputs nOutputs = do
     setSourceRGB 0 0 0
     setLineWidth 1
-    let height = (2*nodeSemiHeight)
+    let height = (nodeSemiHeight*2)
     let topWidth = ((nInputs+1)*nodeGateSpacing)
     let bottomWidth = ((nOutputs+1)*nodeGateSpacing)
 
@@ -61,13 +80,13 @@ drawNode posX posY nInputs nOutputs = do
     doList (\ x -> do
        moveTo (posX-(bottomWidth/2)+x) (posY+nodeSemiHeight)
        relLineTo 0 nodeGateVertOffset) .
-       map (* nodeGateSpacing) $
+       List.map (* nodeGateSpacing) $
        (seqInt nOutputs [])
 
     doList (\ x -> do
        moveTo (posX-(topWidth/2)+x) (posY-nodeSemiHeight)
        relLineTo 0 (-nodeGateVertOffset)) .
-       map (* nodeGateSpacing) $
+       List.map (* nodeGateSpacing) $
        (seqInt nInputs [])
 
 
@@ -125,13 +144,66 @@ drawGraph (OGraph gates nodes edges) presentation = do
           drawGate (outerGateOffset + outerGateSpacing*id) position) .
       indexList
 
-drawScene drawState graph = do
-    g <- liftIO (readMVar graph)
-    drawGraph g (Map.empty)
+findSet predicate = Set.foldl (\ accu elem -> if (predicate elem) then Just elem else accu) Nothing
 
-updateScene drawState graph =
-    
+drawSelection _ NoSelection = return ()
+drawSelection bounds (SelectNode id) =
+    case (findSet (\ (i,_) -> i == id) bounds) of
+     Nothing -> return ()
+     Just (_,(BBox startx starty width height)) -> do
+       setSourceRGB 0.5 0.5 0.5
+       moveTo startx starty
+       relLineTo width 0
+       relLineTo 0 height
+       relLineTo (-width) 0
+       closePath
+       stroke
+drawSelection _ _ = return ()
+
+drawScene drawState gsM = do
+    gs <- liftIO (readMVar gsM)
+    drawGraph (totalGraph gs) (presentation gs)
+    drawSelection (nodeBB gs) (selection gs)
+
+updateScene drawStateM gsM =    
     return True
 
-handleClick drawState graph =
+makeNodeBoundingBox 0 0 x y =
+    BBox (x-nodeSemiHeight) (y-nodeSemiHeight) (2*nodeSemiHeight) (2*nodeSemiHeight)
+
+makeNodeBoundingBox nInputs nOutputs x y =
+    let tot = max nInputs nOutputs in
+    let width = (fromIntegral (tot+1))*nodeGateSpacing in
+    BBox (x - width/2) (y - nodeSemiHeight) width (2*nodeSemiHeight)
+
+inBoundingBox x y (BBox startx starty width height) =
+    (x >= startx && y >= starty && x <= startx+width && y <= starty + height)
+
+handleClick drawStateM gsM drawWidget= do
+    coords <- eventCoordinates
+    st <- liftIO (readMVar drawStateM)
+    if st == DNode then liftIO $ do
+        gs <- liftIO (readMVar gsM)
+        let (OGraph gates nodes edges) = totalGraph gs
+        let pres = presentation gs
+        let newId = maxOId nodes + 1
+        let newGraph = OGraph gates ((newId,(ONode "" [])):nodes) edges
+        let newPres = Map.insert newId coords pres
+        let (x,y) = coords
+        let newNodeBB = Set.insert (newId,makeNodeBoundingBox 0 0 x y) (nodeBB gs)
+        modifyMVar_ gsM (\_ -> return (gs { totalGraph = newGraph,
+                                            presentation = newPres,
+                                            nodeBB = newNodeBB }))
+        widgetQueueDraw drawWidget
+    else if st == DSelect then liftIO $ do
+        gs <- liftIO (readMVar gsM)
+        let (x,y) = coords
+        let searchResult = (Set.foldl (\ fnd (id,bb) -> if (inBoundingBox x y bb) then (Just id) else fnd) Nothing (nodeBB gs))
+        newSelection <- return (case searchResult of
+          Nothing -> NoSelection
+          Just id -> SelectNode id)
+        modifyMVar_ gsM (\_ -> return (gs { selection = newSelection }))
+        widgetQueueDraw drawWidget
+    else
+        liftIO $ putStrLn ("Click handled at position " ++ (show coords))
     return True
--- Main.hs
+++ Main.hs
@@ -7,6 +7,7 @@ import Graphics.UI.Gtk.Gdk.EventM
 import Control.Monad.IO.Class (MonadIO(..))
 import Control.Monad.State
 import Data.Set as Set
+import Data.Map as Map
 import Control.Concurrent.MVar
 import Control.Concurrent.MVar
 
@@ -14,12 +15,17 @@ import DrawGraph
 import OpenGraph
 -- http://projects.haskell.org/gtk2hs/docs/tutorial/glade/
 
-data DrawingState = DSelect | DMoving OId | DNode | DEdge | DDrawing OPath
+
 
 main = do
     initGUI
-    graph <- newMVar (OGraph [(OGate "a" True), (OGate "b" False), (OGate "c" False)]
+    let initGraph = (OGraph [(OGate "a" True), (OGate "b" False), (OGate "c" False)]
                      [] (Set.empty))
+    let initPres = Map.empty
+    let initSelection = NoSelection
+    let initNodeBB = Set.empty
+    let initGateBB = Set.empty
+    graphState <- newMVar (GraphState initGraph initPres initSelection initNodeBB initGateBB)
     drawState <- newMVar DSelect
     builder <- builderNew
     builderAddFromFile builder "interface.glade"
@@ -28,9 +34,9 @@ main = do
 
     on window objectDestroy mainQuit
     widgetAddEvents drawWidget [PointerMotionMask, ButtonPressMask]
-    on drawWidget draw (drawScene drawState graph)
-    on drawWidget motionNotifyEvent (updateScene drawState graph)
-    on drawWidget buttonPressEvent (handleClick drawState graph)
+    on drawWidget draw (drawScene drawState graphState)
+    on drawWidget motionNotifyEvent (updateScene drawState graphState)
+    on drawWidget buttonPressEvent (handleClick drawState graphState drawWidget)
 
     let changeDrawingState newState = modifyMVar_ drawState (\_ -> return newState)
 
